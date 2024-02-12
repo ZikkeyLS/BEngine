6d5321f306cba92454003f8c2706843b8458c7fb
ZikkeyLS <hisaha21@yandex.ru>
2024-02-12 03:14:53 +0300

    Calculate axis properly

--- BEngineCore/Camera.cs
+++ BEngineCore/Camera.cs
@@ -4,25 +4,27 @@ namespace BEngineCore
 {
 	internal class Camera
 	{
+		// This looks like base for transform sort of things
+
 		public Vector3 position = Vector3.Zero;
 		public Vector3 rotation = Vector3.Zero;
 
+		public Vector3 forward { get; private set; }
+		public Vector3 up { get; private set; }
+		public Vector3 right { get; private set; }
 
-		public Vector3 forward { get; private set; } = Vector3.UnitZ * -1f;
-		public Vector3 up { get; } = Vector3.UnitY;
-
-		public float fov = 45.0f;
+		public float fov = 60.0f;
+		public float minRange = 0.01f;
+		public float maxRange = 100f;
 
 		public void Recalculate()
 		{
-			if (rotation.Y > 89.0f)
-				rotation.Y = 89.0f;
-			if (rotation.Y < -89.0f)
-				rotation.Y = -89.0f;
+			Quaternion resultRotation = 
+				Quaternion.CreateFromYawPitchRoll(-rotation.X, rotation.Y, rotation.Z);
 
-			forward = Vector3.Transform(Vector3.UnitZ, Quaternion.
-				CreateFromYawPitchRoll(-rotation.X,
-				rotation.Y, rotation.Z));
+			forward = Vector3.Transform(Vector3.UnitZ, resultRotation);
+			up = Vector3.Transform(Vector3.UnitY, resultRotation); 
+			right = Vector3.Transform(Vector3.UnitX, resultRotation);
 		}
 
 		public Matrix4x4 CalculateViewMatrix()
@@ -33,7 +35,7 @@ namespace BEngineCore
 		public Matrix4x4 CalculateProjectionMatrix(float width, float height)
 		{
 			return Matrix4x4.CreatePerspectiveFieldOfView(float.DegreesToRadians(fov),
-					width / height, 0.1f, 100.0f);
+					width / height,minRange, maxRange);
 		}
 	}
 }
