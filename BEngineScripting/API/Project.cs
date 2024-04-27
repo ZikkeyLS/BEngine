c218c2cbc6be4b7853715f41b4ac4917a14adda0
ZikkeyLS <hisaha21@yandex.ru>
2024-04-27 23:57:42 +0300

    Quick fix

--- BEngineScripting/API/Project.cs
+++ BEngineScripting/API/Project.cs
@@ -3,7 +3,7 @@ namespace BEngine
 {
 	public static class Project
 	{
-		public static bool IsEditor => InternalCalls.ProjectIsRuntime();
-		public static bool IsRuntime => InternalCalls.ProjectIsEditor();
+		public static bool IsEditor => InternalCalls.ProjectIsEditor();
+		public static bool IsRuntime => InternalCalls.ProjectIsRuntime();
 	}
 }
